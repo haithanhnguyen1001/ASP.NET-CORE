c91d1529af27a47275831533bfa68d54880940f9
Nguyễn Hải Thành <nhthanh@MacBook-Pro-cua-Nguyen.local>
2024-10-24 10:05:36 +0700

    fix bug

--- Libraries/SV21T1020712.DomainModels/obj/Debug/net8.0/SV21T1020712.DomainModels.AssemblyInfo.cs
+++ Libraries/SV21T1020712.DomainModels/obj/Debug/net8.0/SV21T1020712.DomainModels.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SV21T1020712.DomainModels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24ab75b1054dfc07d2760e12552c20e40ce5f1ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f774ff153fc7629d040036d41dc2651a5cf2cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SV21T1020712.DomainModels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SV21T1020712.DomainModels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
