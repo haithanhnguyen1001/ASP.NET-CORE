412b8087d5866084295bca1168a467e1d1aab2a4
Nguyễn Hải Thành <nhthanh@192.168.1.9.non-exists.ptr.local>
2024-11-14 10:14:39 +0700

    cham diem lan 1

--- Applications/SV21T1020712.Web/Models/OrderSearchResult.cs
+++ Applications/SV21T1020712.Web/Models/OrderSearchResult.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using SV21T1020712.DomainModels;
 
 namespace SV21T1020712.Web.Models
--- Libraries/SV21T1020712.DomainModels/obj/Debug/net8.0/SV21T1020712.DomainModels.AssemblyInfo.cs
+++ Libraries/SV21T1020712.DomainModels/obj/Debug/net8.0/SV21T1020712.DomainModels.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SV21T1020712.DomainModels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca104121b6efc086c208de200373b8145ebf8b4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92902a407af73fdafec7a8373b5f85fef7d14b24")]
 [assembly: System.Reflection.AssemblyProductAttribute("SV21T1020712.DomainModels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SV21T1020712.DomainModels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
